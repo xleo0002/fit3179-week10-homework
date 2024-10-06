54a36aeb75bd7582386a35c62e33d86a7bb3763c
xleo0002 <111990883+xleo0002@users.noreply.github.com>
2024-10-07 00:11:45 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var vg_1 = "pledge_map.vg.json";
+
+vegaEmbed("#proportional_symbol_map", vg_1).then(function (result) {
+
+}).catch(console.error);
+
+var vg_2 = "population_bar.vg.json";
+
+vegaEmbed("#bar_chart", vg_2).then(function (result) {
+
+}).catch(console.error);
+
+var viz = "viz.vg.json";
+
+vegaEmbed("#viz", viz).then(function (result) {
+
+}).catch(console.error);
